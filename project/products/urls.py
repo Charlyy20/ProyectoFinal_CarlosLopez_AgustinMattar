a37e3c4f0407660ea1284c0073a6db6f7e89e4aa
Carlos <carlosl200400@gmail.com>
2024-05-21 01:38:45 -0300

    Add permission required in admin urls

--- project/products/urls.py
+++ project/products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import permission_required
 from .models import Llanta, Aleron, Spoiler, Intake, Widebody
 from .forms import LlantaCreateForm, AleronCreateForm, SpoilerCreateForm, IntakeCreateForm, WidebodyCreateForm
 
@@ -21,25 +22,25 @@ urlpatterns = [
     path('widebody/<int:pk>/', views.detalle_widebody, name='detalle_widebody'),
 
     #----------------------- Creates -----------------------#
-    path('create_llanta/', views.create_product, { 'form_class': LlantaCreateForm, 'redirect_url': 'products:llanta', 'category': 'llanta'}, name='create_llanta'),
-    path('create_aleron/', views.create_product, {'form_class': AleronCreateForm,'redirect_url': 'products:aleron','category': 'aleron'}, name='create_aleron'),
-    path('create_spoiler/', views.create_product, {'form_class': SpoilerCreateForm,'redirect_url': 'products:spoiler','category': 'spoiler'}, name='create_spoiler'),
-    path('create_intake/', views.create_product, {'form_class': IntakeCreateForm,'redirect_url': 'products:intake','category': 'intake'}, name='create_intake'),
-    path('create_widebody/', views.create_product, {'form_class': WidebodyCreateForm,'redirect_url': 'products:widebody','category': 'widebody'}, name='create_widebody'),
+    path('create_llanta/', permission_required('products.can_create_llanta')(views.create_product), {'form_class': LlantaCreateForm, 'redirect_url': 'products:llanta', 'category': 'llanta'}, name='create_llanta'),
+    path('create_aleron/', permission_required('products.can_create_aleron')(views.create_product), {'form_class': AleronCreateForm, 'redirect_url': 'products:aleron', 'category': 'aleron'}, name='create_aleron'),
+    path('create_spoiler/', permission_required('products.can_create_spoiler')(views.create_product), {'form_class': SpoilerCreateForm, 'redirect_url': 'products:spoiler', 'category': 'spoiler'}, name='create_spoiler'),
+    path('create_intake/', permission_required('products.can_create_intake')(views.create_product), {'form_class': IntakeCreateForm, 'redirect_url': 'products:intake', 'category': 'intake'}, name='create_intake'),
+    path('create_widebody/', permission_required('products.can_create_widebody')(views.create_product), {'form_class': WidebodyCreateForm, 'redirect_url': 'products:widebody', 'category': 'widebody'}, name='create_widebody'),
     
     #----------------------- Update -----------------------#
-    path('update_llanta/<int:pk>/', views.update_product, {'model_class': Llanta,'form_class': LlantaCreateForm,'redirect_url': 'products:llanta'}, name='update_llanta'),
-    path('update_aleron/<int:pk>/', views.update_product, {'model_class': Aleron,'form_class': AleronCreateForm,'redirect_url': 'products:aleron'}, name='update_aleron'),
-    path('update_spoiler/<int:pk>/', views.update_product, {'model_class': Spoiler,'form_class': SpoilerCreateForm,'redirect_url': 'products:spoiler'}, name='update_spoiler'),
-    path('update_intake/<int:pk>/', views.update_product, {'model_class': Intake,'form_class': IntakeCreateForm,'redirect_url': 'products:intake'}, name='update_intake'),
-    path('update_widebody/<int:pk>/', views.update_product, {'model_class': Widebody,'form_class': WidebodyCreateForm,'redirect_url': 'products:widebody'}, name='update_widebody'),
+    path('update_llanta/<int:pk>/', permission_required('products.can_update_llanta')(views.update_product), {'model_class': Llanta, 'form_class': LlantaCreateForm, 'redirect_url': 'products:llanta'}, name='update_llanta'),
+    path('update_aleron/<int:pk>/', permission_required('products.can_update_aleron')(views.update_product), {'model_class': Aleron, 'form_class': AleronCreateForm, 'redirect_url': 'products:aleron'}, name='update_aleron'),
+    path('update_spoiler/<int:pk>/', permission_required('products.can_update_spoiler')(views.update_product), {'model_class': Spoiler, 'form_class': SpoilerCreateForm, 'redirect_url': 'products:spoiler'}, name='update_spoiler'),
+    path('update_intake/<int:pk>/', permission_required('products.can_update_intake')(views.update_product), {'model_class': Intake, 'form_class': IntakeCreateForm, 'redirect_url': 'products:intake'}, name='update_intake'),
+    path('update_widebody/<int:pk>/', permission_required('products.can_update_widebody')(views.update_product), {'model_class': Widebody, 'form_class': WidebodyCreateForm, 'redirect_url': 'products:widebody'}, name='update_widebody'),
     
     #----------------------- Delete -----------------------#
-    path('<int:llanta_id>/delete/', views.delete_llanta, name='delete_llanta'),
-    path('<int:aleron_id>/delete_aleron/', views.delete_aleron, name='delete_aleron'),
-    path('<int:spoiler_id>/delete_spoiler/', views.delete_spoiler, name='delete_spoiler'),
-    path('<int:intake_id>/delete_intake/', views.delete_intake, name='delete_intake'),
-    path('<int:widebody_id>/delete_widebody/', views.delete_widebody, name='delete_widebody'),
+    path('<int:llanta_id>/delete/', permission_required('products.can_delete_llanta')(views.delete_llanta), name='delete_llanta'),
+    path('<int:aleron_id>/delete_aleron/', permission_required('products.can_delete_aleron')(views.delete_aleron), name='delete_aleron'),
+    path('<int:spoiler_id>/delete_spoiler/', permission_required('products.can_delete_spoiler')(views.delete_spoiler), name='delete_spoiler'),
+    path('<int:intake_id>/delete_intake/', permission_required('products.can_delete_intake')(views.delete_intake), name='delete_intake'),
+    path('<int:widebody_id>/delete_widebody/', permission_required('products.can_delete_widebody')(views.delete_widebody), name='delete_widebody'),
     
     #----------------------- Search mejorado -----------------------#    
     path('llantas/', views.product_search, {'model': Llanta, 'template_name': 'products/llanta.html'}, name='llanta_search'),
